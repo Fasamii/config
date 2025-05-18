482d149e8d4c335ba801e0a04f7c9c52ba5c7509
Fasamii <fasami0512@gmail.com>
2025-05-18 21:20:22 +0200

    scrcp & translation & img-to-text

---
+++ util/scripts/update.sh
@@ -0,0 +1,2 @@
+sudo pacman -Syu --noconfirm firefox swww curl git hyprland exa kitty obsidian brightnessctl zsh btop qalculate-qt playerctl bemenu-wayland spotify-launcher man-db grim slurp tesseract-data-pol tesseract-data-eng tesseract wl-clipboard translate-shell
+yay -Syu --noconfirm coppwr
