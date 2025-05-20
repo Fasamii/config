6b11104acf8971c4ef6f19fc4cc6725b12f6b6c0
Fasamii <fasami0512@gmail.com>
2025-05-20 18:15:02 +0200

    screen shot changes + adding dbus notification

--- util/scripts/update.sh
+++ util/scripts/update.sh
@@ -1,2 +1,2 @@
-sudo pacman -Syu --noconfirm firefox swww curl git hyprland exa kitty obsidian brightnessctl zsh btop qalculate-qt playerctl bemenu-wayland spotify-launcher man-db grim slurp tesseract-data-pol tesseract-data-eng tesseract wl-clipboard translate-shell
-yay -Syu --noconfirm coppwr
+sudo pacman -Syu --noconfirm firefox swww curl git hyprland exa kitty obsidian brightnessctl zsh btop qalculate-qt playerctl bemenu-wayland spotify-launcher man-db grim slurp tesseract-data-pol tesseract-data-eng tesseract wl-clipboard translate-shell jq
+yay -Syu --noconfirm coppwr hyprnotify
