dbf6d14c49feb77c0bed8cf570640a0c59869525
Fasamii <fasami0512@gmail.com>
2025-06-11 22:11:28 +0200

    nvim update and fontconfig

--- util/scripts/update.sh
+++ util/scripts/update.sh
@@ -1,2 +1,4 @@
-sudo pacman -Syu --noconfirm firefox swww curl git hyprland exa kitty obsidian brightnessctl zsh btop qalculate-qt playerctl bemenu-wayland spotify-launcher man-db grim slurp tesseract-data-pol tesseract-data-eng tesseract wl-clipboard translate-shell jq ripgrep npm iwd base-devel ttf-fira-code unzip
+sudo pacman -Syu --noconfirm firefox swww curl git hyprland exa kitty obsidian brightnessctl zsh btop qalculate-qt playerctl bemenu-wayland spotify-launcher man-db grim slurp tesseract-data-pol tesseract-data-eng tesseract wl-clipboard translate-shell jq ripgrep npm iwd base-devel ttf-fira-code unzip noto-fonts-emoji
 yay -Syu --noconfirm coppwr hyprnotify
+
+fc-cache -fv # for updating fontconfig cache in case of changing it
