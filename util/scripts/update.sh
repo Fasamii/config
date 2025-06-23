340c046afd17296c8cb2aa3580112914c18a0d6a
Fasamii <fasami0512@gmail.com>
2025-06-23 16:54:48 +0200

    minor changes

--- util/scripts/update.sh
+++ util/scripts/update.sh
@@ -1,4 +1,4 @@
-sudo pacman -Syu --noconfirm firefox swww curl git hyprland exa kitty obsidian brightnessctl zsh btop qalculate-qt playerctl bemenu-wayland spotify-launcher man-db grim slurp tesseract-data-pol tesseract-data-eng tesseract wl-clipboard translate-shell jq ripgrep npm iwd base-devel ttf-fira-code unzip noto-fonts-emoji
+sudo pacman -Syu --noconfirm firefox swww curl git hyprland exa kitty obsidian brightnessctl zsh btop qalculate-qt playerctl bemenu-wayland spotify-launcher man-db grim slurp tesseract-data-pol tesseract-data-eng tesseract wl-clipboard translate-shell jq ripgrep npm iwd base-devel ttf-fira-code unzip noto-fonts-emoji libnotify pandoc tree-sitter tree-sitter-cli imagemagick
 yay -Syu --noconfirm coppwr hyprnotify
 
 fc-cache -fv # for updating fontconfig cache in case of changing it
