12a1171ba00bc239fcc8cc6fc2a134857480a1ad
Fasamii <fasami0512@gmail.com>
2025-06-29 12:06:15 +0200

    wezterm config & .zshrc alias

--- util/scripts/update.sh
+++ util/scripts/update.sh
@@ -1,4 +1,6 @@
-sudo pacman -Syu --noconfirm firefox swww curl git hyprland exa kitty obsidian brightnessctl zsh btop qalculate-qt playerctl bemenu-wayland spotify-launcher man-db grim slurp tesseract-data-pol tesseract-data-eng tesseract wl-clipboard translate-shell jq ripgrep npm iwd base-devel ttf-fira-code unzip noto-fonts-emoji libnotify pandoc tree-sitter tree-sitter-cli imagemagick
+sudo pacman -Syu --noconfirm firefox swww curl git hyprland exa kitty obsidian brightnessctl zsh btop qalculate-qt playerctl bemenu-wayland spotify-launcher man-db grim slurp tesseract-data-pol tesseract-data-eng tesseract wl-clipboard translate-shell jq ripgrep npm iwd base-devel ttf-fira-code unzip noto-fonts-emoji libnotify pandoc tree-sitter tree-sitter-cli imagemagick gdb php
 yay -Syu --noconfirm coppwr hyprnotify
+
+go install github.com/go-delve/delve/cmd/dlv@latest
 
 fc-cache -fv # for updating fontconfig cache in case of changing it
